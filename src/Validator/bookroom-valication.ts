e3206144f54e6fb5f1b7693b59a15ffc9a2f15a1
PratikPatil10 <pratik.patil@techalchemy.com>
2023-06-27 15:51:09 +0530

    Additional Columns are added in booking data

--- src/Validator/bookroom-valication.ts
+++ src/Validator/bookroom-valication.ts
@@ -17,6 +17,8 @@ export class BookMeetRoomValidations {
           date: Joi.date().format("DD/MM/YYYY").required(),
           startTime: Joi.string().pattern(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/).required(),
           endTime: Joi.string().pattern(/^^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/).required(),
+          guests: Joi.string(),
+          description:Joi.string()
         }),
       };
       const meetRoomDetails: any = ctx.request.body;
