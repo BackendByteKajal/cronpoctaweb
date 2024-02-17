e409dea2ef5060be4362c8d593978d4f2cbd9877
Tech Alchemy <kajal.jagtap@techalchemy.com>
2024-02-17 21:15:05 +0530

    cron error

--- src/pages/api/cron/triggeremail.js
+++ src/pages/api/cron/triggeremail.js
@@ -5,7 +5,7 @@ export default apiHandler({
   get: triggerEmailToAdmin,
 });
 
-async function triggerEmailToAdmin(req, res) {cron
+async function triggerEmailToAdmin(req, res) {
   console.log("cron execute.....")
  const clients = await clientRepo.triggerEmail();
   return res.status(200).json(clients);
